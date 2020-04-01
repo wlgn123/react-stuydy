719afc9d19a40be46881b433efc4957672321044
wlgn123 <wlgn567@naver.com>
2020-04-01 19:08:17 +0900

    Init React Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from 'react'; // 리액트를 불러오는 구문
 import './App.css';
 
 function App() {
+  const name = "리액트";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        {name === '리액트' ? (
+          <h1> 리액트 입니다만</h1>
+        ) : (
+          <h2>리액트가 아닙니다만.</h2>
+        )}
+      </div>
+    </>
   );
 }
 
