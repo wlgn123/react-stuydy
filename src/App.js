69facaff2b9b5865da9b3f6d20b2f091bca5e0db
wlgn123 <wlgn567@naver.com>
2020-04-01 19:28:14 +0900

    삼항 연산자 사용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'; // 리액트를 불러오는 구문
-import './App.css';
 
 function App() {
   const name = "리액트";
@@ -12,6 +11,10 @@ function App() {
         ) : (
           <h2>리액트가 아닙니다만.</h2>
         )}
+        
+        {name === '리액트' && <h1>리액트입니다만2</h1>}
+
+        {name === '리액트' ? <h1>리액트입니다만3</h1> : null}
       </div>
     </>
   );
