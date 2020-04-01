c1ab867fe6d653463a3f204e1a4e35bf2b45fa2c
wlgn123 <wlgn567@naver.com>
2020-04-01 19:32:16 +0900

    undefined 처리 테스트

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'; // 리액트를 불러오는 구문
 
 function App() {
+  // 삼항연산자 테스트
   const name = "리액트";
+  
+  // undefined 테스트
+  const test1 = undefined;
 
+  function _undefinedTest() {
+    // 값이 undefined일 수 있는 경우에는 || 연산자로 undefined처리가 가능하다.
+    return test1 || "값이 undefined 입니다";
+  }
+  
   return (
     <>
       <div>
@@ -15,6 +24,10 @@ function App() {
         {name === '리액트' && <h1>리액트입니다만2</h1>}
 
         {name === '리액트' ? <h1>리액트입니다만3</h1> : null}
+      </div>
+
+      <div>
+        {_undefinedTest()}
       </div>
     </>
   );
