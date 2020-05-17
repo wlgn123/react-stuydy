c536756f5916091c8625af0c16ef2e145c52b208
wlgn123 <wlgn567@naver.com>
2020-05-17 17:38:16 +0900

    3.4.2.3 한 컴포넌트에서 useState 여러번 사용하기

--- src/example/_3/MyComponent4_2.js
+++ src/example/_3/MyComponent4_2.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 const Say = () => {
   // useState 사용 시, 스테이트의 초기값 형태를 자유롭게 넣을 수 있음
   const [message, setMessage] = useState('');
+  const [color, setColor] = useState('black');
 
   const onClickEnter = () => setMessage('안녕하세요!');
   const onClickLeave = () => setMessage('안녕히 가세요!');
@@ -15,7 +16,28 @@ const Say = () => {
       <button type="button" onClick={onClickLeave}>
         퇴장
       </button>
-      <h1>{message}</h1>
+      <h1 style={{ color }}>{message}</h1>
+      <button
+        type="button"
+        style={{ color: 'red' }}
+        onClick={() => setColor('red')}
+      >
+        빨간색
+      </button>
+      <button
+        type="button"
+        style={{ color: 'green' }}
+        onClick={() => setColor('green')}
+      >
+        초록색
+      </button>
+      <button
+        type="button"
+        style={{ color: 'blue' }}
+        onClick={() => setColor('blue')}
+      >
+        파란색
+      </button>
     </div>
   );
 };
